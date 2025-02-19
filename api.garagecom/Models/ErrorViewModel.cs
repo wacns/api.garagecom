aa95ea1695a0aae3aea0ca1a7857e01e0c6a07ce
wacns <abody2013mohd@gmail.com>
2025-02-19 19:16:34 +0300

    Add project files.

---
+++ api.garagecom/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace api.garagecom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
